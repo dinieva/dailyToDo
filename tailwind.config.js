bed474dec290d7eb49d9804c80ce81fae0ef8eba
Dinieva Rezeda <dinievarezeda@mail.ru>
2024-12-18 00:05:09 +0300

    correct some path of img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 export default {
-  content: ["./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      slate: colors.slate,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-      pink: colors.pink,
-      cyan: colors.cyan
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            slate: colors.slate,
+            gray: colors.gray,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            pink: colors.pink,
+            cyan: colors.cyan,
+        },
+        extend: {
+            backgroundImage: {
+                'no-tasks': "url('/images/swim.jpg')",
+                filter: "url('/images/filter.svg')",
+                calendar: "url('/images/calendar.svg')",
+                list: "url('/images/list-tick.svg')",
+                trash: "url('/images/trash.svg')",
+            },
+        },
     },
-    extend: {
-      backgroundImage: {
-        'no-tasks': "url('/public/images/swim.jpg')",
-        'filter': "url('/public/images/filter.svg')",
-        'calendar': "url('/public/images/calendar.svg')",
-        'list': "url('/public/images/list-tick.svg')",
-        'trash': "url('/public/images/trash.svg')",
-      }
-    },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
