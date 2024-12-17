999648a6b3149e835e3cd35d3523fa0f7c1a106f
Dinieva Rezeda <dinievarezeda@mail.ru>
2024-12-18 00:59:46 +0300

    add /dailyToDo/ in routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
     // history: createWebHistory(import.meta.env.BASE_URL),
     history: createWebHistory(),
     routes: [
         {
-            path: '/',
+            path: '/dailyToDo/',
             name: 'home',
-            component: () => import('../views/CurrentTasksPage.vue')
-          },
-          {
+            component: () => import('../views/CurrentTasksPage.vue'),
+        },
+        {
             path: '/create',
             component: () => import('../views/CreateTaskPage.vue'),
-          }, 
-          {
+        },
+        {
             path: '/allTasks',
             component: () => import('../views/AllTasksPage.vue'),
-          }, 
-         {
+        },
+        {
             path: '/noTasks',
             name: 'no-tasks',
-            component: () => import('../views/EmptyListPage.vue')
-         }, 
-
+            component: () => import('../views/EmptyListPage.vue'),
+        },
     ],
-})
+});
